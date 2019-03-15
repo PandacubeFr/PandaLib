e60fe994106b55b5a4e36bd64917572050c8fea8
Marc Baloup <marc.baloup@laposte.net>
2019-03-15 19:01:34 +0100

    Add random item selection from Set

--- src/main/java/fr/pandacube/java/util/RandomUtil.java
+++ src/main/java/fr/pandacube/java/util/RandomUtil.java
@@ -2,6 +2,7 @@ package fr.pandacube.java.util;
 
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class RandomUtil {
 
@@ -21,6 +22,25 @@ public class RandomUtil {
 	
 	public static <T> T listElement(List<T> arr) {
 		return arr.get(rand.nextInt(arr.size()));
+	}
+	
+	/**
+	 * Returns a random value from a set.
+	 * 
+	 * May not be optimized (Actually O(n) )
+	 * @param arr
+	 * @return
+	 */
+	public static <T> T setElement(Set<T> set) {
+		if (set.isEmpty())
+			throw new IllegalArgumentException("set is empty");
+		int retI = rand.nextInt(set.size()), i = 0;
+		for (T e : set) {
+			if (retI == i)
+				return e;
+			i++;
+		}
+		throw new RuntimeException("Should never go to this line of code");
 	}
 
 }
