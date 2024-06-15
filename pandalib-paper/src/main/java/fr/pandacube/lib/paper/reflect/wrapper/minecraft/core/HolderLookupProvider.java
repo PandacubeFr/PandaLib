ed0db5391df1a71b41faf7e68bb91adb02a2537c
Marc Baloup <marc.baloup@laposte.net>
2024-06-15 13:29:41 +0200

    Added missing @ConcreteWrapper

--- pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/minecraft/core/HolderLookupProvider.java
+++ pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/minecraft/core/HolderLookupProvider.java
@@ -2,11 +2,13 @@ package fr.pandacube.lib.paper.reflect.wrapper.minecraft.core;
 
 import fr.pandacube.lib.reflect.Reflect;
 import fr.pandacube.lib.reflect.ReflectClass;
+import fr.pandacube.lib.reflect.wrapper.ConcreteWrapper;
 import fr.pandacube.lib.reflect.wrapper.ReflectWrapper;
 import fr.pandacube.lib.reflect.wrapper.ReflectWrapperI;
 
 import static fr.pandacube.lib.util.ThrowableUtil.wrapEx;
 
+@ConcreteWrapper(HolderLookupProvider.__concrete.class)
 public interface HolderLookupProvider extends ReflectWrapperI {
     ReflectClass<?> REFLECT = wrapEx(() -> Reflect.ofClass("net.minecraft.core.HolderLookup.Provider"));
 
--- pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/minecraft/core/RegistryAccess.java
+++ pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/minecraft/core/RegistryAccess.java
@@ -1,12 +1,13 @@
 package fr.pandacube.lib.paper.reflect.wrapper.minecraft.core;
 
-import fr.pandacube.lib.paper.reflect.wrapper.minecraft.network.protocol.custom.CustomPacketPayload;
 import fr.pandacube.lib.reflect.Reflect;
 import fr.pandacube.lib.reflect.ReflectClass;
+import fr.pandacube.lib.reflect.wrapper.ConcreteWrapper;
 import fr.pandacube.lib.reflect.wrapper.ReflectWrapper;
 
 import static fr.pandacube.lib.util.ThrowableUtil.wrapEx;
 
+@ConcreteWrapper(RegistryAccess.__concrete.class)
 public interface RegistryAccess extends HolderLookupProvider {
     ReflectClass<?> REFLECT = wrapEx(() -> Reflect.ofClass("net.minecraft.core.RegistryAccess"));
 
