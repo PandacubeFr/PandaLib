51bc0bd6e8ef6e3b6bbec953a09b211738c4ca24
Marc Baloup <marc.baloup@laposte.net>
2025-01-19 17:53:53 +0100

    Removed unused reflected method.

--- pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/minecraft/world/Entity.java
+++ pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/minecraft/world/Entity.java
@@ -1,6 +1,5 @@
 package fr.pandacube.lib.paper.reflect.wrapper.minecraft.world;
 
-import fr.pandacube.lib.paper.reflect.wrapper.minecraft.nbt.CompoundTag;
 import fr.pandacube.lib.reflect.Reflect;
 import fr.pandacube.lib.reflect.ReflectClass;
 import fr.pandacube.lib.reflect.ReflectMethod;
@@ -12,14 +11,9 @@ import static fr.pandacube.lib.util.ThrowableUtil.wrapReflectEx;
 public class Entity extends ReflectWrapper {
     public static final ReflectClass<?> REFLECT = wrapEx(() -> Reflect.ofClass("net.minecraft.world.entity.Entity"));
     public static final ReflectMethod<?> getBukkitEntity = wrapEx(() -> REFLECT.method("getBukkitEntity")); // spigot method
-    public static final ReflectMethod<?> serializeEntity = wrapEx(() -> REFLECT.method("serializeEntity", CompoundTag.REFLECT.get())); // paper method
 
     public org.bukkit.entity.Entity getBukkitEntity() {
         return (org.bukkit.entity.Entity) wrapReflectEx(() -> getBukkitEntity.invoke(__getRuntimeInstance()));
-    }
-
-    public boolean serializeEntity(CompoundTag nbt) {
-        return wrapReflectEx(() -> (Boolean) serializeEntity.invoke(__getRuntimeInstance(), unwrap(nbt)));
     }
 
     protected Entity(Object obj) {
