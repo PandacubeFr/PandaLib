7d2a5e7862e1ef4f44ced1720cbd87c80eb4e34d
Marc Baloup <marc.baloup@laposte.net>
2025-07-14 00:26:04 +0200

    Fix another reflection wrapper inheritance

--- pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/paper/commands/ShadowBrigNode.java
+++ pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/paper/commands/ShadowBrigNode.java
@@ -1,14 +1,12 @@
 package fr.pandacube.lib.paper.reflect.wrapper.paper.commands;
 
-import com.mojang.brigadier.tree.LiteralCommandNode;
+import fr.pandacube.lib.paper.reflect.wrapper.brigadier.CommandNode;
 import fr.pandacube.lib.reflect.Reflect;
 import fr.pandacube.lib.reflect.ReflectClass;
-import fr.pandacube.lib.reflect.wrapper.ReflectWrapperTyped;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
 
 import static fr.pandacube.lib.util.ThrowableUtil.wrapEx;
 
-public class ShadowBrigNode extends ReflectWrapperTyped<LiteralCommandNode<CommandSourceStack>> {
+public class ShadowBrigNode<S> extends CommandNode<S> {
     public static final ReflectClass<?> REFLECT = wrapEx(() -> Reflect.ofClass("io.papermc.paper.command.brigadier.ShadowBrigNode"));
 
 
