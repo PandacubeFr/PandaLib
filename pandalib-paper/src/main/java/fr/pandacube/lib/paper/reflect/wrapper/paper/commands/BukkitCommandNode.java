c09362c75e5ce48f8753717859948f07dd62857a
Marc Baloup <marc.baloup@laposte.net>
2025-07-14 00:20:00 +0200

    Fix reflection wrapper inheritance

--- pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/paper/commands/BukkitCommandNode.java
+++ pandalib-paper/src/main/java/fr/pandacube/lib/paper/reflect/wrapper/paper/commands/BukkitCommandNode.java
@@ -1,17 +1,15 @@
 package fr.pandacube.lib.paper.reflect.wrapper.paper.commands;
 
-import com.mojang.brigadier.tree.LiteralCommandNode;
+import fr.pandacube.lib.paper.reflect.wrapper.brigadier.CommandNode;
 import fr.pandacube.lib.reflect.Reflect;
 import fr.pandacube.lib.reflect.ReflectClass;
 import fr.pandacube.lib.reflect.ReflectMethod;
-import fr.pandacube.lib.reflect.wrapper.ReflectWrapperTyped;
-import io.papermc.paper.command.brigadier.CommandSourceStack;
 import org.bukkit.command.Command;
 
 import static fr.pandacube.lib.util.ThrowableUtil.wrapEx;
 import static fr.pandacube.lib.util.ThrowableUtil.wrapReflectEx;
 
-public class BukkitCommandNode extends ReflectWrapperTyped<LiteralCommandNode<CommandSourceStack>> {
+public class BukkitCommandNode<S> extends CommandNode<S> {
     public static final ReflectClass<?> REFLECT = wrapEx(() -> Reflect.ofClass("io.papermc.paper.command.brigadier.bukkit.BukkitCommandNode"));
     private static final ReflectMethod<?> getBukkitCommand = wrapEx(() -> REFLECT.method("getBukkitCommand"));
 
