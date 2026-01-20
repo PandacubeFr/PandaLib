281b252b91e5149d1f691d4a0b15ed4d4a9cf876
Marc Baloup <marc.baloup@laposte.net>
2026-01-21 00:41:20 +0100

    Updated WorldUtil to adapt to the new location of the data folder (in DIM* folders when present). Also refactored the code a little.

--- pandalib-paper/src/main/java/fr/pandacube/lib/paper/world/WorldUtil.java
+++ pandalib-paper/src/main/java/fr/pandacube/lib/paper/world/WorldUtil.java
@@ -9,12 +9,22 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  * Provides utility methods to manage and analyze world's directory.
  */
 public class WorldUtil {
 
+
+	/**
+	 * Gets the directory of the provided world.
+	 * @param world the world.
+	 * @return the directory of the world.
+	 */
+	public static File worldDir(String world) {
+		return new File(Bukkit.getWorldContainer(), world);
+	}
 
 	/**
 	 * Determine the {@link Environment} of the provided world.
@@ -48,26 +58,41 @@ public class WorldUtil {
 		throw new IllegalStateException("Unable to determine the type of the world " + world + ".");
 	}
 
+
+	/**
+	 * Determines the directory containing the various region, poi, entities, data directories for the provided world,
+	 * taking into account the environment.
+	 * <ul>
+	 *     <li>For the overworld, this is the main world directory.</li>
+	 *     <li>For the nether, this is the DIM-1 directory.</li>
+	 *     <li>For the end, this is the DIM1 directory.</li>
+	 * </ul>
+	 * @param world the name of the world.
+	 * @throws IllegalStateException if the provided world is not valid (cannot determine its environment)
+	 * @return the environment directory.
+	 */
+	public static File environmentDir(String world) {
+        File worldFolder = worldDir(world);
+		return switch (determineEnvironment(world)) {
+			case NORMAL -> worldFolder;
+			case NETHER -> new File(worldFolder, "DIM-1");
+			case THE_END -> new File(worldFolder, "DIM1");
+			case CUSTOM -> throw new IllegalStateException("The provided world '" + world + "' has a custom Environment type. Unable to determine the environment directory.");
+		};
+
+	}
+
 	/**
 	 * Gets the list of all the regions of the provided world, based on the name of the region files.
 	 * @param world the world.
 	 * @return a {@link List} of {@link RegionCoord}.
 	 */
 	public static List<RegionCoord> getExistingRegions(String world) {
-		Environment env = determineEnvironment(world);
+		File regionDir = new File(environmentDir(world), "region");
 
-		File worldFolder = worldDir(world);
-
-		File file = switch (env) {
-			case NORMAL -> new File(worldFolder, "region");
-			case NETHER -> new File(worldFolder, "DIM-1" + File.separator + "region");
-			case THE_END -> new File(worldFolder, "DIM1" + File.separator + "region");
-            case CUSTOM -> throw new IllegalStateException("The provided world '" + world + "' has a custom Environment type. Unable to tell where the region are stored.");
-        };
-
-		String[] fileList = file.list();
+		String[] fileList = regionDir.list();
 		if (fileList == null) {
-			throw new IllegalStateException("The provided world '" + world + "' does not have a valid region folder (expected '" + file + "').");
+			throw new IllegalStateException("The provided world '" + world + "' does not have a valid region folder (expected '" + regionDir + "').");
 		}
 
 		return Arrays.stream(fileList)
@@ -81,8 +106,6 @@ public class WorldUtil {
 				.filter(Objects::nonNull)
 				.toList();
 	}
-	
-	private static final List<String> REGION_DATA_FILES = Arrays.asList("entities", "poi", "region", "DIM-1", "DIM1");
 
 	/**
 	 * Gets all the directory containing region related data (entities, poi, region, DIM*) of the provided world.
@@ -90,7 +113,20 @@ public class WorldUtil {
 	 * @return a {@link List} of directory.
 	 */
 	public static List<File> regionDataFolders(String world) {
-		return onlyExisting(worldDir(world), REGION_DATA_FILES);
+		File envDir = environmentDir(world);
+		return Stream.of("entities", "poi", "region")
+				.map(f -> new File(envDir, f))
+				.filter(File::exists)
+				.toList();
+	}
+
+	/**
+	 * Gets the data directory of the provided world.
+	 * @param world the world.
+	 * @return the data directory of the world.
+	 */
+	public static File dataDir(String world) {
+        return new File(environmentDir(world), "data");
 	}
 
 	/**
@@ -101,32 +137,7 @@ public class WorldUtil {
 	 */
 	public static List<File> mapFiles(String world) {
 		Pattern mapFilePattern = Pattern.compile("map_\\d+.dat");
-		return List.of(dataDir(world).listFiles((dir, name) -> mapFilePattern.matcher(name).find() || "idcounts.dat".equals(name)));
-	}
-	
-	private static List<File> onlyExisting(File worldDir, List<String> searchList) {
-		return searchList.stream()
-				.map(f -> new File(worldDir, f))
-				.filter(File::exists)
-				.toList();
-	}
-
-	/**
-	 * Gets the directory of the provided world.
-	 * @param world the world.
-	 * @return the directory of the world.
-	 */
-	public static File worldDir(String world) {
-		return new File(Bukkit.getWorldContainer(), world);
-	}
-
-	/**
-	 * Gets the data directory of the provided world.
-	 * @param world the world.
-	 * @return the data directory of the world.
-	 */
-	public static File dataDir(String world) {
-		return new File(worldDir(world), "data");
+		return List.of(Objects.requireNonNull(dataDir(world).listFiles((dir, name) -> mapFilePattern.matcher(name).find() || "idcounts.dat".equals(name))));
 	}
 
 	/**
